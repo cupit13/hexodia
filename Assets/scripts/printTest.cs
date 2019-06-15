ab777e23682a1686ede8d7f79951273dce769e55
Jeff Johar <jjohar@ayzenberg.com>
2019-06-14 17:26:16 -0700

    first commit

---
+++ Assets/scripts/printTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("switchA"))
+        {
+            print("button A is pressed");
+        }
+        if (Input.GetButtonDown("switchB"))
+        {
+            print("button B is pressed");
+        }
+        if (Input.GetButtonDown("switchY"))
+        {
+            print("button Y is pressed");
+        }
+        if (Input.GetButtonDown("switchX"))
+        {
+            print("button X is pressed");
+        }
+
+    }
+}
