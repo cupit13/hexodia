0268ae0746c80803780895c154e49d3ead0c6985
Jeff Johar <jjohar@ayzenberg.com>
2019-06-17 18:09:54 -0700

    added hole prefab, grass prefab, direction when moving and falling trigger

---
+++ Assets/scripts/falling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<movement>().isFalling = true;
+            other.GetComponent<CharacterController>().enabled = false;
+
+            other.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+    }
+}
--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -14,28 +14,74 @@ public class movement : MonoBehaviour
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public bool pcCtrl;
 
     private Vector3 moveDirection = Vector3.zero;
+    public GameObject lookAtTarget;
+    public GameObject rotatedObj;
+    public Vector2 roundedPos;
+    public bool isFalling;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
 
+    //void dirUpdate(float tx,float ty)
+    //{
+    //    float sum = Mathf.Pow(tx,2) + Mathf.Pow(ty, 2);
+    //    float sqrt = Mathf.Sqrt(sum);
+    //    float ry = Mathf.Rad2Deg * Mathf.Acos(tx/sqrt);
+    //    transform.eulerAngles = new Vector3 (0, ry, 0);
+    //}
+
+    Vector2 roundedPosOp(float tx, float tz)
+    {
+        int txR = Mathf.CeilToInt(tx + 4.5f);
+        int tyR = Mathf.CeilToInt(tz + 4.5f);
+        Vector2 res = new Vector2(txR, tyR);
+        return res;
+    }
+
     void Update()
     {
+        roundedPos = roundedPosOp(transform.position.x, transform.position.z);
+
         if (characterController.isGrounded)
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-
-            moveDirection = new Vector3(-Input.GetAxis("switchH"), 0.0f, Input.GetAxis("switchV"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("switchB"))
+            if (Mathf.Abs(moveDirection.x) > 0 || Mathf.Abs(moveDirection.y) > 0 || Mathf.Abs(moveDirection.z) > 0)
             {
-                moveDirection.y = jumpSpeed;
+                lookAtTarget.transform.localPosition = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+                rotatedObj.transform.LookAt(lookAtTarget.transform);
             }
+
+            if (!isFalling)
+            {
+                // We are grounded, so recalculate
+                // move direction directly from axes
+                if (pcCtrl)
+                {
+                    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+                    moveDirection *= speed;
+
+                    if (Input.GetButton("Jump"))
+                    {
+                        moveDirection.y = jumpSpeed;
+                    }
+                }
+                else
+                {
+                    moveDirection = new Vector3(-Input.GetAxis("switchH"), 0.0f, Input.GetAxis("switchV"));
+                    moveDirection *= speed;
+
+                    if (Input.GetButton("switchB"))
+                    {
+                        moveDirection.y = jumpSpeed;
+                    }
+                }
+            }
+
+
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
